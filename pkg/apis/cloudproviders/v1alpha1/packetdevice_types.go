6531b664042ae739822d387ae36bce349d6a61e2
Kazumasa Kohtaka <kkohtaka@gmail.com>
2018-12-22 17:20:28 +0900

    Add a CRD which represents a device on Packet

---
+++ pkg/apis/cloudproviders/v1alpha1/packetdevice_types.go
@@ -0,0 +1,58 @@
+/*
+Copyright 2018 Kazumasa Kohtaka <kkohtaka@gmail.com>.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// PacketDeviceSpec defines the desired state of PacketDevice
+type PacketDeviceSpec struct {
+	ID string `json:"id"`
+}
+
+// PacketDeviceStatus defines the observed state of PacketDevice
+type PacketDeviceStatus struct {
+	Hostname          string   `json:"hostname,omitempty"`
+	PublicIPAddresses []string `json:"publicIPAddresses,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PacketDevice is the Schema for the packetdevices API
+// +k8s:openapi-gen=true
+type PacketDevice struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   PacketDeviceSpec   `json:"spec,omitempty"`
+	Status PacketDeviceStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PacketDeviceList contains a list of PacketDevice
+type PacketDeviceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []PacketDevice `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&PacketDevice{}, &PacketDeviceList{})
+}
---
+++ pkg/controller/packetdevice/packetdevice_controller.go
@@ -0,0 +1,94 @@
+/*
+Copyright 2018 Kazumasa Kohtaka <kkohtaka@gmail.com>.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package packetdevice
+
+import (
+	"context"
+
+	cloudprovidersv1alpha1 "github.com/kkohtaka/namingway/pkg/apis/cloudproviders/v1alpha1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+/**
+* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
+* business logic.  Delete these comments after modifying this file.*
+ */
+
+// Add creates a new PacketDevice Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+// USER ACTION REQUIRED: update cmd/manager/main.go to call this cloudproviders.Add(mgr) to install this Controller
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcilePacketDevice{Client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("packetdevice-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to PacketDevice
+	err = c.Watch(&source.Kind{Type: &cloudprovidersv1alpha1.PacketDevice{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var _ reconcile.Reconciler = &ReconcilePacketDevice{}
+
+// ReconcilePacketDevice reconciles a PacketDevice object
+type ReconcilePacketDevice struct {
+	client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a PacketDevice object and makes changes based on the state read
+// and what is in the PacketDevice.Spec
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=cloudproviders.kohtaka.org,resources=packetdevices,verbs=get;list;watch;create;update;patch;delete
+func (r *ReconcilePacketDevice) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	// Fetch the PacketDevice instance
+	instance := &cloudprovidersv1alpha1.PacketDevice{}
+	err := r.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Object not found, return.  Created objects are automatically garbage collected.
+			// For additional cleanup logic use finalizers.
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	return reconcile.Result{}, nil
+}

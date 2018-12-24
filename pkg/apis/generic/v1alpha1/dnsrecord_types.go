21598f0b3038efadbe71732124ecb31f5b8f182d
Kazumasa Kohtaka <kkohtaka@gmail.com>
2018-12-24 23:31:11 +0900

    Add CRDs for DNS records

---
+++ pkg/apis/generic/v1alpha1/dnsrecord_types.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2018 Kazumasa Kohtaka <kkohtaka@gmail.com>.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DNSRecordSpec defines the desired state of DNSRecord
+type DNSRecordSpec struct {
+	Domain string   `json:"domain"`
+	A      []string `json:"a"`
+}
+
+// DNSRecordStatus defines the observed state of DNSRecord
+type DNSRecordStatus struct {
+	Ready bool `json:"ready"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// DNSRecord is the Schema for the dnsrecords API
+// +k8s:openapi-gen=true
+type DNSRecord struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DNSRecordSpec   `json:"spec,omitempty"`
+	Status DNSRecordStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// DNSRecordList contains a list of DNSRecord
+type DNSRecordList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []DNSRecord `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&DNSRecord{}, &DNSRecordList{})
+}
